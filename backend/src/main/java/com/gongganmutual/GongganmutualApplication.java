bcd6c680c6c2619b6a36314567bebce551f5fe79
KIM-CHOONGHOON <chunggiwa@gmail.com>
2025-07-05 23:02:40 +0900

    front,back end 환경 설정 1차완료 (db연결까지)

---
+++ backend/src/main/java/com/gongganmutual/GongganmutualApplication.java
@@ -0,0 +1,13 @@
+package com.gongganmutual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongganmutualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongganmutualApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gongganmutual/GongganmutualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongganmutual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongganmutualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
